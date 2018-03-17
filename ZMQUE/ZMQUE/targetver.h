8abe319cd293194ed285897400878be81e527306
David Kaplan <Jin-Vyral@users.noreply.github.com>
2018-03-17 03:37:55 -0400

    Added banner text

--- ZMQUE/ZMQUE/targetver.h
+++ ZMQUE/ZMQUE/targetver.h
@@ -1,3 +1,14 @@
+// ZMQUE: ZMQ for UE4 by David Kaplan
+//
+// LEGAL NOTICE:
+// This project and its contents are provided without warranty and support
+// will not be provided.This project and its contents exist within the
+// public domain.Any derivations or use cases can attribute the author
+// and the Git repository at the discretion of their owners, but any
+// acknowledgement is appreciated.
+//
+// Github repository: https://github.com/Jin-VE/ZMQUE.git
+
 #pragma once
 
 // Including SDKDDKVer.h defines the highest available Windows platform.
