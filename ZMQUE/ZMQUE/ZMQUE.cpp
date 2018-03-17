ee1d025ee04182f2387ced4261897237dd651b65
David Kaplan <Jin-Vyral@users.noreply.github.com>
2018-03-17 03:35:59 -0400

    Added banner text

--- ZMQUE/ZMQUE/ZMQUE.cpp
+++ ZMQUE/ZMQUE/ZMQUE.cpp
@@ -1,4 +1,15 @@
-﻿#include "stdafx.h"
+// ZMQUE: ZMQ for UE4 by David Kaplan
+//
+// LEGAL NOTICE:
+// This project and its contents are provided without warranty and support
+// will not be provided.This project and its contents exist within the
+// public domain.Any derivations or use cases can attribute the author
+// and the Git repository at the discretion of their owners, but any
+// acknowledgement is appreciated.
+//
+// Github repository: https://github.com/Jin-VE/ZMQUE.git
+
+#include "stdafx.h"
 #include "ZMQUE.h"
 
 void* ZMQ_ctx_new()
